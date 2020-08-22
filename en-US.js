aed014399f5a89126910a939ebad7453f6b57fc1
Aashir Aamir Khan <accounts@justaashir.com>
2020-08-23 04:18:41 +0500

    Add files via upload

---
+++ en-US.js
@@ -0,0 +1,45 @@
+export default {
+  dashboard: {
+    message: {
+      hello: "Hello",
+      message: "here’s how your business is performing today"
+    },
+    counts: {
+      todayRevenue: "Today's Revenue",
+      todaySales: "Today's Sales",
+      checkoutPageUsers: {
+        checkoutPageUsers: 'Checkout Page Users',
+        live: 'Live'
+      },
+    },
+    stats: {
+      stats: 'Stats',
+      filterBy: {
+        filterBy: 'Filter By',
+        type: ['Nuxt', 'Green', 'Vue'],
+        date: {
+          filters: ['Last 7 Days', 'Last 14 Days', 'Last 30 Days'],
+          from: 'From',
+          to: 'To',
+          custom: 'Custom'
+        },
+      },
+      chart: {
+        totalRevenue: 'Total Revenue',
+        sales: 'Sales'
+      },
+      performance: {
+        performance: 'Performance',
+        launchApproval: 'Launch Approval',
+        refund: 'Refund',
+        chargeBack: 'Chargeback'
+      },
+      bankPaymentSlip: {
+        bankPaymentSlip: 'Bank Payment Slip',
+        issued: 'Issued',
+        paid: 'Paid',
+        conversion: 'conversion'
+      },
+    },
+  }
+}
---
+++ pt-BR.js
@@ -0,0 +1,45 @@
+export default {
+  dashboard: {
+    message: {
+      hello: 'Olá',
+      message: 'veja como está o desempenho de sua empresa hoje'
+    },
+    counts: {
+      todayRevenue: "Receita de Hoje",
+      todaySales: "Vendas de Hoje",
+      checkoutPageUsers: {
+        checkoutPageUsers: 'Usuários Caixa Página',
+        live: 'Live'
+      },
+    },
+    stats: {
+      stats: 'Estatísticos',
+      filterBy: {
+        filterBy: 'Filtrar por',
+        type: ['Nuxt', 'Green', 'Vue'],
+        date: {
+          filters: ['Últimos 7 dias', 'Últimos 14 dias', 'Últimos 30 dias'],
+          from: 'De',
+          to: 'Para',
+          custom: 'Personalizados'
+        },
+      },
+      chart: {
+        totalRevenue: 'Total receita',
+        sales: 'Vendas'
+      },
+      performance: {
+        performance: 'Desempenho',
+        launchApproval: 'aprovação de lançamento',
+        refund: 'Restituição',
+        chargeBack: 'Estorno'
+      },
+      bankPaymentSlip: {
+        bankPaymentSlip: 'Boleto bancário',
+        issued: 'Emitida',
+        paid: 'Pago',
+        conversion: 'conversão'
+      },
+    },
+  }
+}
